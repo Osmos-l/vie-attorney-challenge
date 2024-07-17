38e275b581a317510fd8341a17ee4cde63e786a7
Jordan LECLERCQ <emailjeux81@gmail.com>
2024-07-17 11:55:39 +0200

    feat - wip : Attorney API

--- pages/api/attorney-data/index.js
+++ pages/api/attorney-data/index.js
@@ -1,31 +1,36 @@
 import dbConnect from '@/utils/dbConnect'
 import Attorney from '@/db-schemas/Attorney'
+import responseHandler from '@/utils/responseHandler';
+
+const getAttorneys = async (params, res) => {
+  try {
+    const attorneys = await Attorney.find({});
+    return responseHandler.success(200, attorneys, res);
+  } catch (error) {
+    return responseHandler.error(error, res);
+  }
+}
+
+const createAttorney = async(body, res) => {
+  try {
+    const attorney = await Attorney.create(body);
+    return responseHandler.success(201, attorney, res);
+  } catch (error) {
+    return responseHandler.error(error, res);
+  }
+}
 
 export default async function handler(req, res) {
-  const { method } = req
+  const { method, body, params } = req;
 
   await dbConnect()
 
   // To be completed
   switch (method) {
-    case 'GET':
-      try {
-        const attorneys = await Attorney.find({})
-        res.status(200).json({ success: true, data: attorneys })
-      } catch (error) {
-        res.status(400).json({ success: false })
-      }
-      break
-    case 'POST':
-      try {
-        const attorney = await Attorney.create(req.body)
-        res.status(201).json({ success: true, data: attorney })
-      } catch (error) {
-        res.status(400).json({ success: false })
-      }
-      break
+    case 'GET': return getAttorneys(params, res);
+    case 'POST': return createAttorney(body, res);
     default:
-      res.status(400).json({ success: false })
+      res.status(400).json({ success: false });
       break
   }
 }
---
+++ utils/responseHandler.js
@@ -0,0 +1,24 @@
+/**
+ * Build correctly the success response
+ * @param {*} code the code response
+ * @param {*} data data to be send
+ * @param {*} res 
+ * @returns response built
+ */
+const success = (code, data, res) => {
+    return res.status(code).json({ success: true, data });
+}
+  
+/**
+ * Build correctly the error response
+ * @param {*} error The error thrown
+ * @param {*} res 
+ * @returns response built
+ */
+const error = (error, res) => {
+    // TODO: Log the error into dashboard IT team
+  
+    return res.status(400).json({ success: false });
+}
+
+export default { success, error };
