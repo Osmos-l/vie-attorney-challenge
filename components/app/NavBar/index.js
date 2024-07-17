e2a80bac7874930d304fc6409b3f5dfd95c645aa
Jordan LECLERCQ <emailjeux81@gmail.com>
2024-07-17 17:06:45 +0200

    feat: Adding navbar

---
+++ components/app/NavBar/index.js
@@ -0,0 +1,42 @@
+'use client'
+
+import { AppBar, Box, Button, Container, Divider, List, ListItem, ListItemButton, ListItemText, rgbToHex, Toolbar, Typography } from "@mui/material";
+import { usePathname } from "next/navigation";
+
+const pages = [
+    { name: "Home",         path: "/"},
+    { name: "Attorneys",    path: "/attorneys-panel"},
+    { name: "Violations",   path: "/violations-panel"}
+];
+
+export const NavBar = () => {
+    const pathname = usePathname();
+
+    return (
+        <AppBar position="static">
+            <Toolbar 
+                sx={{ display: { xs: "block", md: "flex" } }}
+            >
+                <Typography
+                   variant="h6"
+                   component="div"
+                   sx={{ display: "block", textAlign: "center" }}
+                >
+                    CRUD
+                </Typography>
+                <Divider />
+                <Box sx={{ display: "block" }}>
+                    {pages.map(({ name, path }) => (
+                        <Button 
+                            key={name}
+                            href={path} 
+                            sx={{ color: pathname === path ? "rgba(17, 25, 39, 0.38)" : "white" }}
+                        >
+                            {name}
+                        </Button>
+                    ))}
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,14 +3,17 @@ import { Provider as MobxProvider } from 'mobx-react'
 import mobxStore from '@/stores/AppStore'
 // Theme
 import ThemeProvider from '@/stores/providers/ThemeProvider'
+import { NavBar } from '@/components/app/NavBar'
+import { Box, Drawer } from '@mui/material'
 
 // Global Styles
 
 const App = ({ Component, pageProps }) => {
   return (
-    <MobxProvider store={mobxStore}>
-      <ThemeProvider>
-        <Component {...pageProps} />
+      <MobxProvider store={mobxStore}>
+        <ThemeProvider>
+          <NavBar />
+          <Component {...pageProps} />
       </ThemeProvider>
     </MobxProvider>
   )
