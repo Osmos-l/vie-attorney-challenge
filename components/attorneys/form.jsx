f9f578a65760a2a145528bc57c0c2c3f1ff60b3f
Jordan LECLERCQ <emailjeux81@gmail.com>
2024-07-19 14:02:11 +0200

    feat: Adding SkeletonPricesMap on /edit attorney

--- components/attorneys/form.jsx
+++ components/attorneys/form.jsx
@@ -1,9 +1,10 @@
 import { inject, observer } from 'mobx-react';
 import React, { useEffect, useState } from 'react';
-import { Box, TextField, Button, Typography, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Box, TextField, Button, Typography, Select, MenuItem, FormControl, InputLabel, Divider } from '@mui/material';
 import { useRouter } from 'next/router';
 
 import Alert from '@mui/material/Alert';
+import SkeletonAttorneyPricesMapListPanel from '../attorneyPriceMap/skeletonListPanel';
 
 const AttorneyForm = ({ initialData, attorneyStore }) => {
     const [id, setId] = useState(null);
@@ -60,84 +61,93 @@ const AttorneyForm = ({ initialData, attorneyStore }) => {
     };
 
     return (
-        <Box
-            component="form"
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 2,
-                maxWidth: 400,
-                margin: '0 auto',
-                mt: 5,
-            }}
-            onSubmit={handleSubmit}
-        >
-            {
-                success ? (
-                    <Alert severity="success">
-                        Attorney { id ? "updated" : "created " } successfully !
-                    </Alert>
-                ) : (<></>)
-            } 
-            {
-                error ? (
-                    <Alert severity="error">
-                        An error occured, please try again.
-                    </Alert>
-                ) : (<></>)
-            }
-        <Typography variant="h5" component="div" sx={{ mb: 2 }}>
-            {id ? `${name} Form` : 'Attorney Form'}
-        </Typography>
-        <TextField
-            label="Name"
-            name="name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            variant="outlined"
-            fullWidth
-            required
-        />
-        <TextField
-            label="Address"
-            name="contactAddress"
-            value={contactAddress}
-            onChange={e => setContactAddress(e.target.value)}
-            variant="outlined"
-            type="address"
-            fullWidth
-            required
-        />
-        <TextField
-            label="Phone"
-            name="contactPhone"
-            value={contactPhone}
-            onChange={e => setContactPhone(e.target.value)}
-            variant="outlined"
-            type="phone"
-            fullWidth
-            required
-        />
-        <TextField
-            label="Email"
-            name="contactEmail"
-            value={contactEmail}
-            onChange={e => setContactEmail(e.target.value)}
-            variant="outlined"
-            type="email"
-            fullWidth
-            required
-        />
-        <Button variant="contained" color="primary" type="submit">
-            Submit
-        </Button>
-        <Button 
-            variant="outlined" 
-            color="secondary"
-            onClick={handleCancel}
-        >
-            Cancel
-        </Button>
+        <Box>
+            <Box
+                component="form"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                    maxWidth: 400,
+                    margin: '0 auto',
+                    mt: 5,
+                }}
+                onSubmit={handleSubmit}
+            >
+                {
+                    success ? (
+                        <Alert severity="success">
+                            Attorney { id ? "updated" : "created " } successfully !
+                        </Alert>
+                    ) : (<></>)
+                } 
+                {
+                    error ? (
+                        <Alert severity="error">
+                            An error occured, please try again.
+                        </Alert>
+                    ) : (<></>)
+                }
+            <Typography variant="h5" component="div" sx={{ mb: 2 }}>
+                {id ? `${name} Form` : 'Attorney Form'}
+            </Typography>
+            <TextField
+                label="Name"
+                name="name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                variant="outlined"
+                fullWidth
+                required
+            />
+            <TextField
+                label="Address"
+                name="contactAddress"
+                value={contactAddress}
+                onChange={e => setContactAddress(e.target.value)}
+                variant="outlined"
+                type="address"
+                fullWidth
+                required
+            />
+            <TextField
+                label="Phone"
+                name="contactPhone"
+                value={contactPhone}
+                onChange={e => setContactPhone(e.target.value)}
+                variant="outlined"
+                type="phone"
+                fullWidth
+                required
+            />
+            <TextField
+                label="Email"
+                name="contactEmail"
+                value={contactEmail}
+                onChange={e => setContactEmail(e.target.value)}
+                variant="outlined"
+                type="email"
+                fullWidth
+                required
+            />
+            <Button variant="contained" color="primary" type="submit">
+                Submit
+            </Button>
+            <Button 
+                variant="outlined" 
+                color="secondary"
+                onClick={handleCancel}
+            >
+                Cancel
+            </Button>
+            
+        </Box>
+        { id ? (
+            <>
+                <Divider sx={{marginY: 2}} />
+                <SkeletonAttorneyPricesMapListPanel /> 
+            </>
+        ) : '' }
     </Box>
   )
 
